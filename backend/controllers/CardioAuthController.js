687a33823d2501fc5dc5bdef46c176211383193e
mustafa <mustafawarind03@gmail.com>
2025-05-14 14:13:27 +0500

    create cardio db with specific user

--- backend/controllers/CardioAuthController.js
+++ backend/controllers/CardioAuthController.js
@@ -2,10 +2,13 @@ const {Users,Cardio} = require("../models/index");
 
 // Function to create a new cardio workout
 exports.createCardio = async (req, res) => {
-  const { workoutType, cardioName, date, duration, distance, caloriesBurned } = req.body;
-  const userID = req.Users._id // Assuming you have user ID from the token
 
   try {
+    // Check if the user is authenticated
+    console.log("User ID from token:", req.users);
+  const { workoutType, cardioName, date, duration, distance, caloriesBurned } = req.body;
+  const userID = req.users.id; // Assuming you have user ID from the token
+
     const newCardio = await Cardio.create({
       workoutType,
       cardioName,
@@ -15,9 +18,12 @@ exports.createCardio = async (req, res) => {
       caloriesBurned,
       userID
     });
+    // Save the new cardio workout to the database
+    console.log("cardio workout created:", newCardio);
 
     res.status(201).json({ message: "Cardio workout created successfully", newCardio });
   } catch (err) {
+    console.log("Error creating cardio workout:", err);
     res.status(500).json({ error: "Server error" });
   }
 };
