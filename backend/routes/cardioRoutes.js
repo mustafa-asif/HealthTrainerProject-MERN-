7a433abaa67f681a3b226096c5ca1a27b03b7f3d
mustafa <mustafawarind03@gmail.com>
2025-05-14 14:14:13 +0500

    define create route with middle ware

--- backend/routes/cardioRoutes.js
+++ backend/routes/cardioRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { createCardio } = require('../controllers/CardioAuthController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Route to create a new cardio workout
-router.post('/user/login/cardio/create', createCardio);
-router.post('/user/register/cardio/create', createCardio);
+router.post('/create',authMiddleware, createCardio);
+// router.post('/user/register/cardio/create', createCardio);
 // Route to get all cardio workouts 
 
 module.exports = router;
