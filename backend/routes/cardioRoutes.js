fc92df188ce3dc6678e9a4227882fdb20289fb69
mustafa <mustafawarind03@gmail.com>
2025-05-15 16:09:09 +0500

    route set for update and delete function

--- backend/routes/cardioRoutes.js
+++ backend/routes/cardioRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { createCardio,getCardioWorkouts } = require('../controllers/CardioAuthController');
+const { createCardio,getCardioWorkouts,deleteCardio,updateCardio } = require('../controllers/CardioAuthController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 
@@ -13,6 +13,11 @@ router.post('/create',authMiddleware,upload.single('image'), createCardio);
 
 // Route to get all cardio workouts for a user
 router.get('/all', authMiddleware, getCardioWorkouts);
+
+// Route to delete a cardio workout
+router.delete('/delete/:id', authMiddleware, deleteCardio);
+// Route to update a cardio workout
+router.put('/update/:id', authMiddleware, upload.single('image'), updateCardio);
 
 
 module.exports = router;
