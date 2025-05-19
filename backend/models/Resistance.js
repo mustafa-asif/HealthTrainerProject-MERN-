0adb08eb6a7e4b826d4ae58f2ae10b70024d95b4
mustafa <mustafawarind03@gmail.com>
2025-05-19 14:19:55 +0500

    Resistance Model designed

---
+++ backend/models/Resistance.js
@@ -0,0 +1,40 @@
+const {Schema, model} = require('mongoose');
+
+const resistanceSchema = new Schema({
+  workoutType: {
+    type: String,
+    required: true,
+  },
+  resistanceName: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  sets: {
+    type: Number,
+    required: true,
+  },
+  reps: {
+    type: Number,
+    required: true,
+  },
+  weightUsed: {
+    type: Number,
+    required: true,
+  },
+  caloriesBurned: {
+    type: Number,
+    required: true,
+  },
+  userID: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Resistance = model('Resistance', resistanceSchema);
+module.exports = Resistance;
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,7 @@
 const Users = require("./Users");
 const Cardio = require("./Cardio");
+const Resistance = require("./Resistance");
 
 
 // Export the models
-module.exports={Users,Cardio};
+module.exports={Users,Cardio,Resistance};
